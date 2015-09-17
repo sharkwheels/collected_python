c1c1975f91336fb78d1c870fc53452e14b2c827a
Nadine Lessio <idoru00@gmail.com>
2015-09-17 19:58:38 -0400

    just adding some stuff to flagger

--- twitterFlagTwo.py
+++ twitterFlagTwo.py
@@ -1,16 +1,17 @@
-import requests, serial 
+import requests, serial, re
 from twython import Twython
 from twython import TwythonStreamer
 
-# you might want to shove this in a config file. I'm lazy. 
+### GET KEYS (LOCAL) ######################################
 
-APP_KEY = ""
-APP_SECRET = ""
+keys = []
+with open('twitter.txt','r') as my_file:
+    keys = my_file.read().splitlines()
 
-# Access Tokens
-
-OAUTH_TOKEN = ""
-OAUTH_TOKEN_SECRET = ""
+TWIT_KEY = keys[0]
+TWIT_SECRET = keys[1]
+OAUTH_TOKEN = keys[2]
+OAUTH_TOKEN_SECRET = keys[3]
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 twitter.verify_credentials()
@@ -21,9 +22,21 @@ ser = serial.Serial('/tmp/tty.LightBlue-Bean', 9600, timeout=0.25)
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
-            print data['text'].encode('utf-8')
+
+            bodyOf = data['text'].encode('utf-8')
+            command = "hello"
+            acct = '@SydneyTheCat'
+
             print data['user']['screen_name'].encode('utf-8')
-            ser.write("T")
+            
+            if bodyOf.startswith(acct) and command in bodyOf:
+                bodyStrip = re.sub(r'[^\w\s]','',bodyOf.lower())
+                ser.write("T")
+            elif bodyOf.startswith(acct) and command not in bodyOf:
+                print 'sorry no command'
+            else:
+                print "neither thing was found"
+
 
     def on_error(self, status_code, data):
         print status_code
@@ -33,5 +46,4 @@ class MyStreamer(TwythonStreamer):
         # self.disconnect()
 
 stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-stream.statuses.filter(track='@_nadine') #kinda hacky, but it works. This is filtering the public stream and just looking for '@_nadine', it can look for anything. 
+stream.statuses.filter(track='@SydneyTheCat') 
